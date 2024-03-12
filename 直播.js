b6ed2a11f8a54d3c82e877d95b635d2c709c6168
caca3559823437 <47878307+caca3559823437@users.noreply.github.com>
2024-03-12 17:15:24 +0800

    Update 直播.js

--- 直播.js
+++ 直播.js
@@ -1,22 +1,13 @@
 ﻿[
- 
 {"name": "新贰叁直播",     "url": "https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/2023.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
 {"name": "麻豆直播",     "url": "https://p.ntnas.top/https://raw.githubusercontent.com/123linyao123/TV/main/MiTV0.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
 {"name": "电视直播",     "url": "https://agit.ai/397260863/1/raw/branch/master/0.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
 {"name": "MV合集",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
- 
 {"name": "MTV合集",     "url": "https://qu.ax/KmzB.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
-
 {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
-
 {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
-
 {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
-
 {"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
-
 {"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"},
-
 {"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt&&&https://p.ntnas.top/https://raw.githubusercontent.com/caca3559823437/2022/main/VOL_93.jpg"}
-
 ]
